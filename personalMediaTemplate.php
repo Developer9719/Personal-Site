e7254314b847f06c840cf5faecb9a324d0452eb6
John Doe <justin.bat19@icloud.com>
2023-08-04 00:54:44 -0400

    Updated Comments

--- personalMediaTemplate.php
+++ personalMediaTemplate.php
@@ -4,26 +4,16 @@
 */
 get_header();
 
-$errorMessages = [];
+// Upload Media Form Processor
 
-if(empty($_POST)) {
-  array_push($errorMessages, 'Form not submitted.');
-} 
-
-if(!empty($errorMessages)) {
-  foreach ($errorMessages as $error) {
-    echo '<script>alert("' . $error . ', ")</script>';
-  }
-}
 ?>
 
 <h1 class="title"><?php echo get_the_title(); ?></h1>
 
-<form action="" method="post">
-  <label>Upload Media: </label>
-  <input type="file" multiple name="files[]"></input>
-  <button type="submit">Upload Now</button>
-<form>
+<!-- Upload Media Form -->
+
+
+<!-- Display current media -->
 
 <?php
 get_footer();
