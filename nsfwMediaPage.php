567324f164d69c62bc6be752f69bfb00ece55b39
John Doe <justin.bat19@icloud.com>
2023-08-09 12:51:57 -0400

    Update NSFW Media Collection Page

--- nsfwMediaPage.php
+++ nsfwMediaPage.php
@@ -9,3 +9,4 @@ get_header();
 ?>
 
 <h1 class="title"><?php echo get_the_title(); ?></h1>
+<p>Adult media is allowed on site by Dreamhost as long as it is legal. So only add images of adult actors</p>
