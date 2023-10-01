040f864bd36a06437fc153d1a90df30fd2a2af2d
John Doe <justin.bat19@icloud.com>
2023-09-30 21:56:03 -0400

    Updated files

--- Local Python Apps to Interface with Server/autoUpload.py
+++ Local Python Apps to Interface with Server/autoUpload.py
@@ -1,15 +1,1 @@
 # A program that automatically uploads any and all media saved to the Pictures folder to my website (into a specific folder for Uploads allowing me to organize it as I feel)
-
-serverDirectory = '/home/dh_9mx6wk/funandpractice.dreamhosters.com/wp-content/themes/astra-child/Media'
-webAddress = 'https://funandpractice.dreamhosters.com/personal-media/'
-newUploadDirectory = serverDirectory + '/New Uploads'
-
-fileToUpload = 'D:\iCloud Photos\Other\Wallpaper\Celebrities\Emily Kinney\16686725876374265150625656653281.png'
-
-from paramiko import SSHClient
-from scp import SCPClient
-ssh = SSHClient()
-ssh.load_system_host_keys()
-ssh.connect('dh_9mx6wk@iad1-shared-d12-03.dreamhost.com:/home/dh_9mx6wk/funandpractice.dreamhosters.com/wp-content/themes/astra-child/Media/New Uploads')
-with SCPClient(ssh.get_transport()) as scp:
-    scp.put(fileToUpload) # Copy my_file.txt to the server
--- portfolio.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Template Name: Portfolio Page
- */
-get_header();
-echo '<h1>' . single_post_title() . '</h1>';
-get_footer();
-?>
